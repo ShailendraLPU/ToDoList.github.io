e371f03b42f1a6bae3e5a22e014e9da71c9e2d46
Shailendra Srivastava <66545888+ShailendraLPU@users.noreply.github.com>
2021-04-03 15:32:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,50 +2,42 @@ const todobutton = document.querySelector('#button1');
 const todoinput = document.querySelector('input');
 const todoitem  = document.querySelector('ul');
 
-
- todobutton.addEventListener('click',()=> {
-    if(todoinput.value!="")
-    {
-        const div = document.createElement('div');
- const li = document.createElement('li');
- const p1 = document.createElement('p');
- p1.innerText = todoinput.value;
- li.appendChild(p1);
+const head  = document.querySelector('nav i');
+head.addEventListener('click',()=>{
+   
+    const sec = document.querySelector('section');
+    sec.classList.toggle('section1');
+    const tog = document.querySelector('section>div input');
+   
+    tog.classList.toggle('visible');
  
- const div2 = document.createElement('div');
- const edit = document.createElement('i');
- edit.classList.add('fas', 'fa-edit');
- const dlt = document.createElement('i');
- dlt.classList.add('fas', 'fa-trash-alt');
- const check = document.createElement('i');
- check.classList.add('fas', 'fa-check');
 
- div2.appendChild(check);
- div2.appendChild(edit);
- div2.appendChild(dlt);
-
- li.append(div2);
- li.classList.add('list');
+})
 
 
+const  check = document.querySelectorAll('ul div li div i:nth-of-type(1)');
+for(const ev of check){
+ev.addEventListener('click',(e) =>{
 
-div.appendChild(li);
-todoitem.append(div);
-todoinput.value = "";
+    e.target.parentElement.parentElement.children[0].classList.toggle('lineth');
 
-dlt.addEventListener('click',(e) =>{
+});
+}
 
-    e.target.parentElement.parentElement.parentElement.classList.add('fall');
-    e.target.parentElement.parentElement.parentElement.addEventListener('transitionend',() =>  e.target.parentElement.parentElement.parentElement.remove());
-  
-} );
+
+const  edit = document.querySelectorAll('ul div li div i:nth-of-type(2)');
 
 let count = 0;
-edit.addEventListener('click',(e) =>
+for(const ev of edit)
+{
+ev.addEventListener('click',(e) =>
  {
+    
+   
+  
     if(count!=0)
     {
-        console.log("if");
+       
         const edited = document.createElement('p');
         const val =  e.target.parentElement.parentElement.children[0].value;
         e.target.parentElement.parentElement.children[0].replaceWith(edited);
@@ -54,7 +46,7 @@ edit.addEventListener('click',(e) =>
     }
     else
     {
-        console.log("else");
+      
         const edited = document.createElement('input');
         const val =  e.target.parentElement.parentElement.children[0].innerText;
         e.target.parentElement.parentElement.children[0].replaceWith(edited);
@@ -64,20 +56,21 @@ edit.addEventListener('click',(e) =>
    
 });
 
-check.addEventListener('click',(e) =>{
+}
 
-     e.target.parentElement.parentElement.children[0].classList.toggle('lineth');
 
-});
+const  dlt = document.querySelectorAll('ul div li div i:nth-of-type(3)');
 
-    }
+for(const ev of dlt)
+{
+ev.addEventListener('click',(e) =>{
 
-    else{
-        todoinput.setAttribute('placeholder','First Write then Add');
-    }
-   
+  
+    e.target.parentElement.parentElement.parentElement.classList.add('fall');
+    e.target.parentElement.parentElement.parentElement.addEventListener('transitionend',() =>  e.target.parentElement.parentElement.parentElement.remove());
+  
+} )};
 
-});
 
 
 todoinput.addEventListener('keypress',(event)=> {
@@ -134,7 +127,7 @@ todoinput.addEventListener('keypress',(event)=> {
   {
     if(count!=0)
     {
-        console.log("if");
+     
         const edited = document.createElement('p');
         const val =  e.target.parentElement.parentElement.children[0].value;
         e.target.parentElement.parentElement.children[0].replaceWith(edited);
@@ -143,7 +136,7 @@ todoinput.addEventListener('keypress',(event)=> {
     }
     else
     {
-        console.log("else");
+      
         const edited = document.createElement('input');
         const val =  e.target.parentElement.parentElement.children[0].innerText;
         e.target.parentElement.parentElement.children[0].replaceWith(edited);
@@ -167,8 +160,6 @@ todoinput.addEventListener('keypress',(event)=> {
    
 
 });
-
-
 
 
 
